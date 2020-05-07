1089d695b8c68e7a5737a363b6fb8797d37b43be
MinexAutomation <minex.automation@gmail.com>
2020-05-07 12:03:48 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Karlstad.Base.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Karlstad.Base.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Karlstad.Base/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Karlstad.Base
+{
+    public class Class1
+    {
+    }
+}
